68fb292fae73abde0e21f002deb87d8e57373b07
Johnny Ruiz <jeko2000@yandex.com>
2018-04-27 15:23:22 -0400

    Remove scripts folder and its cron manipulation scripts
    Now that we removed the cron hooks for Gnus via f32289d, we no longer
    need the respective scripts

--- scripts/start-mail-cron-job.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-SPOOL_DIR=/var/spool/cron
-
-echo "*/2 * * * * /usr/bin/offlineimap >> $HOME/mail/logs/offlineimap.log" > $SPOOL_DIR/$USER
--- scripts/stop-mail-cron-job.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-SPOOL_DIR=/var/spool/cron
-
-rm $SPOOL_DIR/$USER
