4f89e4bd14acd23e92b6197cda290d2a5f5a8d84
Johnny Ruiz <jeko2000@yandex.com>
2018-06-25 12:35:58 -0500

    Update list of package-selected-packages in custom.el

--- custom.el
+++ custom.el
@@ -14,7 +14,7 @@
  '(cua-overwrite-cursor-color "#b58900")
  '(cua-read-only-cursor-color "#859900")
  '(custom-safe-themes
-   '("31992d4488dba5b28ddb0c16914bf5726dc41588c2b1c1a2fd16516ea92c1d8e" "12b204c8fcce23885ce58e1031a137c5a14461c6c7e1db81998222f8908006af" default))
+   '("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "31992d4488dba5b28ddb0c16914bf5726dc41588c2b1c1a2fd16516ea92c1d8e" "12b204c8fcce23885ce58e1031a137c5a14461c6c7e1db81998222f8908006af" default))
  '(fci-rule-color "#073642")
  '(highlight-changes-colors '("#d33682" "#6c71c4"))
  '(highlight-symbol-colors
@@ -40,7 +40,7 @@
  '(nrepl-message-colors
    '("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4"))
  '(package-selected-packages
-   '(pinentry py-autopep8 hide-mode-line org-present counsel swiper clojure-mode iy-go-to-char magit ivy yasnippet-snippets yasnippet company emms org-plus-contrib org frame-cmds frame-fns 0blayout pdf-tools nov geiser slime-company common-lisp-snippets commonlisp-snippets column-marker deferred rainbow-delimiters impatient-mode markdown-mode zoom-frm zenburn-theme which-key web-mode waher-theme w3m visual-regexp-steroids use-package undo-tree transpose-frame tangotango-theme swiper-helm sr-speedbar solarized-theme smooth-scrolling smex smartscan smartparens slime skewer-mode shell-pop scratch restclient quickrun python-mode perspective pdb-mode password-store paredit org-pandoc olivetti nlinum neotree multiple-cursors multi-term monokai-theme material-theme magit-gitflow less-css-mode jdee ido-vertical-mode hydra hindent helm-swoop helm-projectile helm-gtags helm-ag hayoo haskell-mode gradle-mode gnus-desktop-notify ggtags flycheck expand-region emmet-mode elpy edit-server drag-stuff disable-mouse cyberpunk-theme crux counsel-projectile company-c-headers command-log-mode bongo bbdb arjen-grey-theme android-mode alert ace-window ace-jump-mode ac-html ac-cider 4clojure))
+   '(pydoc-info company-box sql-indent company-auctex smart-mode-line pinentry py-autopep8 hide-mode-line org-present counsel swiper clojure-mode iy-go-to-char magit ivy yasnippet-snippets yasnippet company emms org-plus-contrib org frame-cmds frame-fns 0blayout pdf-tools nov geiser slime-company common-lisp-snippets commonlisp-snippets column-marker deferred rainbow-delimiters impatient-mode markdown-mode zoom-frm zenburn-theme which-key web-mode waher-theme w3m visual-regexp-steroids use-package undo-tree transpose-frame tangotango-theme swiper-helm sr-speedbar solarized-theme smooth-scrolling smex smartscan smartparens slime skewer-mode shell-pop scratch restclient quickrun python-mode perspective pdb-mode password-store paredit org-pandoc olivetti nlinum neotree multiple-cursors multi-term monokai-theme material-theme magit-gitflow less-css-mode jdee ido-vertical-mode hydra hindent helm-swoop helm-projectile helm-gtags helm-ag hayoo haskell-mode gradle-mode gnus-desktop-notify ggtags flycheck expand-region emmet-mode edit-server drag-stuff disable-mouse cyberpunk-theme crux counsel-projectile company-c-headers command-log-mode bongo bbdb arjen-grey-theme android-mode alert ace-window ace-jump-mode ac-html ac-cider 4clojure))
  '(pos-tip-background-color "#073642")
  '(pos-tip-foreground-color "#93a1a1")
  '(safe-local-variable-values
