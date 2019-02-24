c7308439244b77d2f142c02c2bf1154877c7bc70
Johnny Ruiz <jeko2000@yandex.com>
2019-02-24 13:49:41 -0500

    Override bongo and zoom-frm configuration

--- early-overrides.el
+++ early-overrides.el
@@ -3,3 +3,12 @@
 (let ((dir "/data/rep/personal/org"))
   (if (file-exists-p dir)
       (setq jr/org-directory dir)))
+
+;; Zoom frm
+(setq jr/zoom-frm-desired-zoom 1)
+
+
+;; Bongo
+(setq jr/bongo-music-directory "/data/music/")
+(setq jr/volume-raise-volume-command "pactl set-sink-volume @DEFAULT_SINK@ +5%")
+(setq jr/volume-lower-volume-command "pactl set-sink-volume @DEFAULT_SINK@ -5%")
